fda7bcf178084a9def7027a8509b3de66ef819af
Girish-Bhargava <109140966+Girish-Bhargava@users.noreply.github.com>
2022-07-26 03:16:53 +0530

    Create file1.py

---
+++ .github/workflows/file1.py
@@ -0,0 +1,15 @@
+import os 
+
+def main():
+  res=os.environ.get("result")
+  k = os.environ.get("k1")
+  
+  if res.get('k') == None:
+    res[k]=value
+    
+
+  
+  
+if __name__=='__main__':
+  main()
+  
