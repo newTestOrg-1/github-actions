e561b321ea995b60cf9a7cd933067fb518177569
Girish-Bhargava <109140966+Girish-Bhargava@users.noreply.github.com>
2022-07-26 10:37:22 +0530

    Update file2.py

--- .github/workflows/file2.py
+++ .github/workflows/file2.py
@@ -1,1 +1,15 @@
+import os 
 
+def main():
+  res=os.environ.get("result")
+  k = os.environ.get("k1")
+  
+  for i in no_of_key_to_add
+    if res.get( ) == None:
+    
+    
+
+  
+  
+if __name__=='__main__':
+  main()
